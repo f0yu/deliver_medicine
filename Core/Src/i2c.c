2ad6c0171b2f5715fe03323d85b652517804a029
foyu <2312706700@qq.com>
2023-11-12 13:44:14 +0800

    add freertos

--- Core/Src/i2c.c
+++ Core/Src/i2c.c
@@ -56,7 +56,35 @@ void MX_I2C1_Init(void)
 
 }
 
+void HAL_I2C_MspInit(I2C_HandleTypeDef* i2cHandle)
+{
 
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(i2cHandle->Instance==I2C1)
+  {
+  /* USER CODE BEGIN I2C1_MspInit 0 */
+
+  /* USER CODE END I2C1_MspInit 0 */
+
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    /**I2C1 GPIO Configuration
+    PB8     ------> I2C1_SCL
+    PB9     ------> I2C1_SDA
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_8|GPIO_PIN_9;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    __HAL_AFIO_REMAP_I2C1_ENABLE();
+
+    /* I2C1 clock enable */
+    __HAL_RCC_I2C1_CLK_ENABLE();
+  /* USER CODE BEGIN I2C1_MspInit 1 */
+
+  /* USER CODE END I2C1_MspInit 1 */
+  }
+}
 
 void HAL_I2C_MspDeInit(I2C_HandleTypeDef* i2cHandle)
 {
